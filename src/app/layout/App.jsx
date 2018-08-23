80cd96c9eeb7f603d8a48931c1f05260698659a1
Ariful Islam <arifjmamun24@gmail.com>
2018-08-24 01:20:25 +0600

    Configuring and Implementing Routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'; 
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
-import { Container } from 'semantic-ui-react';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import EventForm from '../../features/event/EventForm/EventForm';
+import SettingsDashboard from '../../features/user/settings/SettingsDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import HomePage from '../../features/home/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        
+        <Route exact path="/(.+)" render={() => (
+          <div>
+            <NavBar />
+            <Container className="main">
+              <Switch>              
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/event/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/events/create" component={EventForm} />
+              </Switch>
+            </Container>
+          </div>
+        )}/>        
       </div>
     );
   }
---
+++ src/features/user/settings/BasicsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BasicsPage = () => {
+  return (
+    <div>
+      Basics Page
+    </div>
+  );
+};
+
+export default BasicsPage;

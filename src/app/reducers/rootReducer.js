bf60afd915e339273d4923fe2d030cc46520fa4c
Md. Ariful Islam <arifjmamun24@gmail.com>
2018-08-29 01:59:42 +0600

    Added Toastr for notification.

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
-import { reducer as FormReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
+import { reducer as toastrReducer } from 'react-redux-toastr';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 import modalsReducer from '../../features/modals/modalsReducer';
@@ -7,12 +8,13 @@ import authReducer from '../../features/auth/authReducer';
 import asyncReducer from '../../features/async/asyncReducer';
 
 const rootReducer = combineReducers({
-  form: FormReducer,
+  form: formReducer,
   test: testReducer,
   events: eventReducer,
   modals: modalsReducer,
   auth: authReducer,
-  async: asyncReducer
+  async: asyncReducer,
+  toastr: toastrReducer,
 });
 
 export default rootReducer;
--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,3 +1,4 @@
+import { toastr } from 'react-redux-toastr';
 import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT, FETCH_EVENT } from './eventConstants';
 import { asyncActionStarted, asyncActionFinished, asyncActionError } from '../async/asyncActions';
 import { fetchSampleData } from '../../app/data/mockApi';
@@ -10,23 +11,44 @@ export const fetchEvents = (events) => {
 };
 
 export const createEvent = (event) => {
-  return {
-    type: CREATE_EVENT,
-    payload: { event }
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: CREATE_EVENT,
+        payload: { event }
+      });
+      toastr.success('Success!', 'Event has been created');
+    } catch (error) {
+      toastr.error('Oops!', 'Something went wrong');
+    }
   };
 };
 
 export const updateEvent = (event) => {
-  return {
-    type: UPDATE_EVENT,
-    payload: { event }
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: UPDATE_EVENT,
+        payload: { event }
+      });
+      toastr.success('Success!', 'Event has been updated');
+    } catch (error) {
+      toastr.error('Oops!', 'Something went wrong');
+    }
   };
 };
 
 export const deleteEvent = (eventId) => {
-  return {
-    type: DELETE_EVENT,
-    payload: { eventId }
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: DELETE_EVENT,
+        payload: { eventId }
+      });
+      toastr.success('Success!', 'Event has been removed');
+    } catch (error) {
+      toastr.error('Oops!', 'Something went wrong');
+    }
   };
 };
 
